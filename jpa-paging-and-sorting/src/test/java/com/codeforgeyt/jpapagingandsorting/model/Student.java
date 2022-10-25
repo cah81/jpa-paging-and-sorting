e2459c4602c06330fbd8056f6596d59539af0fcc
carlos Angulo <carlosenrique.angulo@bosonit.com>
2022-10-25 08:25:59 +0200

    Initial commit

---
+++ jpa-paging-and-sorting/src/test/java/com/codeforgeyt/jpapagingandsorting/model/Student.java
@@ -0,0 +1,46 @@
+package com.codeforgeyt.jpapagingandsorting.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "STUDENT")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+
+
+    public Student() {
+    }
+
+    public Student(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+}
